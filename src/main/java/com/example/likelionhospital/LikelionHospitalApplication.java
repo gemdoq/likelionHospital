a6eca9b4fcb5a93fc041e1e7c941b82813a90b47
gemdoq <gemdoq@gmail.com>
2022-12-08 14:46:43 +0900

    feature: New Create Project of likelionHospital

---
+++ src/main/java/com/example/likelionhospital/LikelionHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionHospitalApplication.class, args);
+    }
+
+}
